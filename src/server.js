f40839bcbef78ca388c30ef148a608d9c397164a
Ksenia Jarjury <100300372+KseniaJarjury@users.noreply.github.com>
2022-11-20 15:04:50 +0100

    socket

--- src/server.js
+++ src/server.js
@@ -53,6 +53,10 @@ app.use('/contratooferente', require('./routes/contratooferente'));
 app.use('/formcontratacion', require('./routes/formcontratacion'));
 app.use('/perfiloferente', require('./routes/perfiloferente'));
 
-app.listen(port, () => {
+let server = app.listen(port, () => {
   console.log(`Listening on port ${port}`)
+});
+server.on('clientError', (err, socket) => {
+  console.error(err);
+  socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
 });
