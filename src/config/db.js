04df322784b7ad0c828ae28a0e0f99cc99bd7591
Ksenia Jarjury <100300372+KseniaJarjury@users.noreply.github.com>
2022-11-20 12:47:51 +0100

    connection

--- src/config/db.js
+++ src/config/db.js
@@ -3,11 +3,11 @@ const mysql = require('mysql2');
 
 // create the connection to database
 const connectionSync = mysql.createConnection({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_DATABASE,
-    user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST || localhost,
+    port: process.env.DB_PORT || 3306,
+    database: process.env.DB_DATABASE || servyar,
+    user: process.env.DB_USERNAME || root,
+    password: process.env.DB_PASSWORD || "",
 });
 
 const connectionAsync = connectionSync.promise();
