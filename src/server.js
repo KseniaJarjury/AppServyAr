f6b1d6c6abe412cb7c833da3a55130eb3b449bf7
Ksenia Jarjury <100300372+KseniaJarjury@users.noreply.github.com>
2022-11-20 13:43:49 +0100

    .

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const ejs = require('ejs');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const dotenv = require('dotenv');
 dotenv.config();
@@ -54,5 +54,5 @@ app.use('/formcontratacion', require('./routes/formcontratacion'));
 app.use('/perfiloferente', require('./routes/perfiloferente'));
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Listening on port ${port}`)
 });
