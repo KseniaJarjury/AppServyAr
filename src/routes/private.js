fb82ce0daa7c56a0b60149b84ed3b7f53d024385
Maria Belen Ursino <belu.ursino@icloud.com>
2022-11-28 21:00:12 -0300

    Funcionalidades agregadas: Contrato

--- src/routes/private.js
+++ src/routes/private.js
@@ -157,4 +157,64 @@ router.get('/rol', (req, res) => {
 //     // res.render('register');
 // });
 
+//////////////////////CONTRATO
+
+const modelContrato = require('../models/contrato');
+
+router.get('/', async (req, res) => {
+     let propuesta = await modelContrato.getAll();
+    // let contratos = {
+    //     "Id_contrato": 1,
+    //     "Id_servicio": 1,
+    //     "Id_usuario": 1,
+    // };
+    res.render('contrato', {
+        contratos: propuesta,
+    })
+});
+
+
+router.get('/contrato/:Id_propuesta', async (req, res) => {
+    const propuesta = await modelContrato.find(req.params.Id_propuesta);
+    res.render('contrato', {
+        contratos: propuesta,
+    });
+});
+
+
+
+router.post('', async (req, res) => {
+    console.log(req.body);
+    res.send('generando contrato');
+});
+
+
+///CONT. OFERENTE
+
+router.get('/', async (req, res) => {
+    let propuesta = await modelContrato.getAll();
+   // let contratos = {
+   //     "Id_contrato": 1,
+   //     "Id_servicio": 1,
+   //     "Id_usuario": 1,
+   // };
+   res.render('contratooferente', {
+       contratos: propuesta,
+   })
+});
+
+
+router.get('/contrato/:Id_propuesta', async (req, res) => {
+   const propuesta = await modelContrato.find(req.params.Id_propuesta);
+   res.render('contratooferente', {
+       contratos: propuesta,
+   });
+});
+
+
+//////////////FIN CONTRATO
+
+
+
+
 module.exports = router;
